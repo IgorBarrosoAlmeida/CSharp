dad26ffbfb17537dd64a453bd0f2075f88f5e231
Igor Barroso Almeida <igorba867@gmail.com>
2023-01-11 14:28:47 -0300

    Start bytebank project

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+namespace bytebank {
+    public class ContaCorrente{
+        // Atributos
+        public string titular;
+        public string id;
+        public int agencia;
+        public double saldo;
+
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bytebank {
+    class Program {
+        public static void Main(string[] args) {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.id = "140b";
+            Console.WriteLine(conta.id);
+        }
+    }
+}
