b54dc9597823263d3ac71ec455db04c8f2de19e8
Igor Barroso Almeida <igorba867@gmail.com>
2023-03-06 18:04:02 -0300

    add array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,18 +1,42 @@
 ﻿using bytebank.Modelos.Conta;
 using System;
+using bytebank_ATENDIMENTO.bytebank.util;
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+/*
+void TestaArrayContaCorrente() {
+    ContaCorrente[] contas = new ContaCorrente[]
+    {
+        new ContaCorrente(888, "584923-A"),
+        new ContaCorrente(879, "584923-B"),
+        new ContaCorrente(877, "584923-C")
+    };
 
-ContaCorrente[] contas = new ContaCorrente[]
-{
-    new ContaCorrente(888, "584923-A"),
-    new ContaCorrente(879, "584923-B"),
-    new ContaCorrente(877, "584923-C")
-};
+    int cont = 0;
 
-int cont = 0;
-
-foreach(var conta in contas) {
-    Console.WriteLine($"Conta[{cont}]: {conta.Id}");
-    cont++;
+    foreach(var conta in contas) {
+        Console.WriteLine($"Conta[{cont}]: {conta.Id}");
+        cont++;
+    }
 }
+*/
+
+void TestaArrayContaCorrente() {
+    ListaDeContasCorrentes array = new ListaDeContasCorrentes();
+
+    // Adiciona contas
+    array.addConta(new ContaCorrente(888, "584923-A"));
+    array.addConta(new ContaCorrente(879, "584923-B"));
+    array.addConta(new ContaCorrente(877, "584923-C"));
+    array.addConta(new ContaCorrente(855, "584923-D"));
+    array.addConta(new ContaCorrente(866, "584923-E"));
+    array.addConta(new ContaCorrente(866, "584923-E"));
+    array.addConta(new ContaCorrente(888, "584923-A"));
+    array.addConta(new ContaCorrente(879, "584923-B"));
+    array.addConta(new ContaCorrente(877, "584923-C"));
+    array.addConta(new ContaCorrente(855, "584923-D"));
+
+    array.printArray();
+}
+
+TestaArrayContaCorrente();
---
+++ bytebank_ATENDIMENTO/bytebank.util/ListaDeContasCorrentes.cs
@@ -0,0 +1,41 @@
+using bytebank.Modelos.Conta;
+
+namespace bytebank_ATENDIMENTO.bytebank.util {
+    public class ListaDeContasCorrentes {
+
+        private ContaCorrente[] _contas = null;
+        private int _proximaPosicao = 0;
+
+        public ListaDeContasCorrentes(int tamanhoInicial=5) {
+            this._contas = new ContaCorrente[tamanhoInicial];
+        }
+
+        public void addConta(ContaCorrente conta) {
+            if(this._contas.Length == this._proximaPosicao) {
+                this.aumentarArray(this._contas.Length + 1);
+            }
+            this._contas[this._proximaPosicao] = conta;
+            this._proximaPosicao++;
+        }
+
+        public void aumentarArray(int tamanhoNecessario) {
+            ContaCorrente[] arrayNovo = new ContaCorrente[tamanhoNecessario];
+            
+            // Copia as contas já existentes para o array novo
+            for(int i = 0; i < this._contas.Length; i++) {
+                arrayNovo[i] = this._contas[i];
+            }
+
+            // Passa o array com mais posições pro como array do objeto
+            this._contas = arrayNovo;
+        }
+
+        public void printArray() {
+            foreach(var conta in _contas) {
+                System.Console.WriteLine($"Num Agencia: {conta.Numero_agencia}");
+                System.Console.WriteLine($"id: {conta.Id}");
+                System.Console.WriteLine("");
+            }
+        }
+    }
+}
