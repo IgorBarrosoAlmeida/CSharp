30cf10b6477f735d84084e6aa0d41ebfefde678f
Igor Barroso Almeida <igorba867@gmail.com>
2023-03-07 18:02:53 -0300

    add menu

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,7 +1,9 @@
 ﻿using bytebank.Modelos.Conta;
 using System;
+using System.Collections;
 using bytebank_ATENDIMENTO.bytebank.util;
 
+#region Exemplo 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
 void TestaArrayContaCorrente() {
@@ -24,20 +26,112 @@ void TestaClasseArrayDeContas() {
     ListaDeContasCorrentes array = new ListaDeContasCorrentes();
 
     // Adiciona contas
-    array.AddConta(new ContaCorrente(888, "584923-A", 323));
-    array.AddConta(new ContaCorrente(879, "584923-B", 2000));
-    array.AddConta(new ContaCorrente(877, "584923-C", 650));
-    array.AddConta(new ContaCorrente(855, "584923-D", 5));
+    ContaCorrente c1 = new ContaCorrente(888, "584923-A", 3254);
+    ContaCorrente c2 = new ContaCorrente(879, "584923-B", 2022);
+    ContaCorrente c3 = new ContaCorrente(877, "584923-C", 654);
+    ContaCorrente c4 = new ContaCorrente(855, "584923-D", 50);
+    ContaCorrente c5 = new ContaCorrente(700, "584923-E", 5012);
+
+    array.AddConta(c1);
+    array.AddConta(c2);
+    array.AddConta(c3);
+    array.AddConta(c4);
+    array.AddConta(c5);
+    /*
     array.AddConta(new ContaCorrente(866, "584923-E", 3000));
     array.AddConta(new ContaCorrente(866, "584923-E", 5));
     array.AddConta(new ContaCorrente(888, "584923-A", 3000));
     array.AddConta(new ContaCorrente(879, "584923-B", 1230));
     array.AddConta(new ContaCorrente(877, "584923-C", 123));
     array.AddConta(new ContaCorrente(855, "584923-D", 1000));
+    */
+    
+    Console.WriteLine("======================");
+    array.PrintArray();
 
+    Console.WriteLine("Maior Saldo " + (array.MaiorSaldo()).Saldo);
+
+    array.Remover(c3);
+    
+    // Printa info
+    Console.WriteLine("======================");
+    array.PrintArray();
+
+    
+    array.Remover(c2);
+    
+    // Printa info
+    Console.WriteLine("======================");
     array.PrintArray();
 
     Console.WriteLine("Maior Saldo " + (array.MaiorSaldo()).Saldo);
 }
 
-TestaClasseArrayDeContas();
+//TestaClasseArrayDeContas();
+#endregion
+
+
+ArrayList listaDeContas = new ArrayList();
+void Atendimento() {
+    char opcao = '0';
+
+    while(opcao != '6') {
+        Console.Clear();
+        Console.WriteLine("===============================");
+        Console.WriteLine("===       Atendimento       ===");
+        Console.WriteLine("===1 - Cadastrar Conta      ===");
+        Console.WriteLine("===2 - Listar Contas        ===");
+        Console.WriteLine("===3 - Remover Conta        ===");
+        Console.WriteLine("===4 - Ordenar Contas       ===");
+        Console.WriteLine("===5 - Pesquisar Conta      ===");
+        Console.WriteLine("===6 - Sair do sistema      ===");
+        opcao = Console.ReadLine()[0];
+        switch(opcao) {
+            case '1': 
+                CadastrarConta();
+                break;
+            case '2':
+            case '3':
+            case '4': 
+            case '5': 
+            case '6': break;
+            default: 
+                Console.WriteLine("Numero invalido");
+                break;
+        }
+    }
+}
+
+void CadastrarConta() {
+    Console.Clear();
+    Console.WriteLine("===============================");
+    Console.WriteLine("===   CADASTRO DE CONTAS    ===");
+    Console.WriteLine("===============================");
+    Console.WriteLine("\n");
+    Console.WriteLine("=== Informe dados da conta ===");
+    Console.Write("Número da conta: ");
+    string numeroConta = Console.ReadLine();
+
+    Console.Write("Número da Agência: ");
+    int numeroAgencia = int.Parse(Console.ReadLine());
+
+    ContaCorrente conta = new ContaCorrente(numeroAgencia, numeroConta);
+
+    Console.Write("Informe o saldo inicial: ");
+    conta.Saldo = double.Parse(Console.ReadLine());
+
+    Console.Write("Infome nome do Titular: ");
+    conta.Titular.Nome = Console.ReadLine();
+
+    Console.Write("Infome CPF do Titular: ");
+    conta.Titular.Cpf = Console.ReadLine();
+
+    Console.Write("Infome Profissão do Titular: ");
+    conta.Titular.Profissao = Console.ReadLine();
+
+    listaDeContas.Add(conta);
+    Console.WriteLine("... Conta cadastrada com sucesso! ...");
+    Console.ReadKey();
+}
+
+Atendimento();
--- bytebank_ATENDIMENTO/bytebank.util/ListaDeContasCorrentes.cs
+++ bytebank_ATENDIMENTO/bytebank.util/ListaDeContasCorrentes.cs
@@ -12,22 +12,49 @@ namespace bytebank_ATENDIMENTO.bytebank.util {
 
         public void AddConta(ContaCorrente conta) {
             if(this._contas.Length == this._proximaPosicao) {
-                this.AumentarArray(this._contas.Length + 1);
+                this.MudaTamanhoArray(this._contas.Length + 1);
             }
             this._contas[this._proximaPosicao] = conta;
             this._proximaPosicao++;
         }
 
-        public void AumentarArray(int tamanhoNecessario) {
-            ContaCorrente[] arrayNovo = new ContaCorrente[tamanhoNecessario];
+        public void MudaTamanhoArray(int novoTamanho) {
+            ContaCorrente[] arrayNovo = new ContaCorrente[novoTamanho];
             
             // Copia as contas já existentes para o array novo
-            for(int i = 0; i < this._contas.Length; i++) {
-                arrayNovo[i] = this._contas[i];
+            if(novoTamanho >= this._contas.Length) {
+                for(int i = 0; i < this._contas.Length; i++) {
+                    arrayNovo[i] = this._contas[i];
+                }
+            } else {
+                for(int i = 0; i < novoTamanho; i++) {
+                    arrayNovo[i] = this._contas[i];
+                }
             }
 
             // Passa o array com mais posições pro como array do objeto
             this._contas = arrayNovo;
+        }
+
+        public void Remover(ContaCorrente conta) {
+            int indice = 0;
+
+            // Busca o indice da conta que deve ser removida
+            foreach(ContaCorrente c in this._contas) {
+                if(c == conta) {
+                    break;
+                } else {
+                    indice++;
+                }
+            }
+
+            // Atualiza as posições
+            for(int i = indice; i < this._contas.Length-1; i++) {
+                _contas[i] = _contas[i + 1];
+            }
+            this._proximaPosicao--;
+            this._contas[this._proximaPosicao] = null;
+            this.MudaTamanhoArray(this._contas.Length - 1);
         }
 
         public void PrintArray() {
@@ -53,6 +80,22 @@ namespace bytebank_ATENDIMENTO.bytebank.util {
                 return conta;
             } else {
                 return null;
+            }
+        }
+
+        public ContaCorrente RecuperarContaIndice(int indice) {
+            if(indice < 0 || indice >= this._proximaPosicao) {
+                throw new ArgumentOutOfRangeException(nameof(indice));
+            }
+
+            return this._contas[indice];
+        }
+        
+        public int Tamanho {get;}
+
+        public ContaCorrente this[int indice] {
+            get {
+                return this.RecuperarContaIndice(indice);
             }
         }
     }
