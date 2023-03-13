2b236a53b81342469d0989198c5a2ac94ac4d22a
Igor Barroso Almeida <igorba867@gmail.com>
2023-03-12 21:41:31 -0300

    modfy Program.cs

--- ByteBank_IO/Program.cs
+++ ByteBank_IO/Program.cs
@@ -4,7 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        var diretorioArquivo = "./contas.txt";
+        var fluxoDoArquivo = new FileStream(diretorioArquivo, FileMode.Open);
 
+        var buffer = new byte[1024]; // 1 KB
+
+        fluxoDoArquivo.Read(buffer, 0, 1024);
         Console.ReadLine();
     }
 }
