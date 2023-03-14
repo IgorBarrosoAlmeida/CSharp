3e06a950b89785ca6a87baa5768b8b7830c65bc1
Igor Barroso Almeida <igorba867@gmail.com>
2023-03-13 21:22:57 -0300

    Rename folders

---
+++ ByteBank_IO/Program.cs
@@ -0,0 +1,11 @@
+﻿using ByteBankIO;
+
+partial class Program
+{
+    static void Main(string[] args) {
+        //CriarArquivo();
+        
+        CriarArquivoComWriter();
+        Console.ReadKey();
+    }
+}
